545cd928b95e9bf0704b89743c7ad58d0f696d0d
BryFReed <bfr7011@nyu.edu>
2023-04-23 20:16:19 -0400

    addign json stuffto app

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express"
 import { rps, rpsls } from "./lib/rpsls.js"
 const args = minimist(process.argv.slice(2));
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/app/", (rep, res) => {
     res.status(200).send("200 OK")
